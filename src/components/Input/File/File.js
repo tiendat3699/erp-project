fe300e729a7b922ef66a252d2876050160e439d7
DESKTOP-KMNS09Q\Admin <tiendat361999@gmail.com>
2023-03-11 22:11:34 +0700

    update file comp

--- src/components/Input/File/File.js
+++ src/components/Input/File/File.js
@@ -1,29 +1,44 @@
 import classNames from 'classnames/bind';
 
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './File.module.scss';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-function File() {
+function File({ multiple, accept }) {
+    const [drag, setDrag] = useState(false);
+
     const handleDrag = (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        if (e.dataTransfer) {
-            console.log(e.dataTransfer.files);
+        if (e.type === 'dragenter') {
+            setDrag(true);
+        }
+
+        if (e.type === 'dragleave') {
+            setDrag(false);
         }
     };
 
     return (
-        <>
-            <div className={cx('dragable')} draggable></div>
-            <div
-                className={cx('wrapper')}
-                onDragOver={(e) => e.preventDefault()}
-                onDragLeave={(e) => e.preventDefault()}
-                onDragEnter={(e) => e.preventDefault()}
-                onDrop={handleDrag}
-            ></div>
-        </>
+        <div className={cx('wrapper', { drag })}>
+            <input
+                type="file"
+                className={cx('input')}
+                onDragOver={handleDrag}
+                onDragLeave={handleDrag}
+                onDragEnter={handleDrag}
+                multiple={multiple}
+                accept={accept}
+            />
+            <div className={cx('inner')}>
+                <div className={cx('icon')}>
+                    <FontAwesomeIcon icon={faDownload} />
+                    <span className={cx('text')}>Chọn file hoặc kéo vào đây</span>
+                </div>
+            </div>
+        </div>
     );
 }
 
